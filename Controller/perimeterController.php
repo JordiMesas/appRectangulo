5c97a8861479386b37aa8efdd7ca1bb216145a63
Jordi <jordimesas54gmail.com>
2020-10-02 11:14:42 +0200

    add appRectangulo project

---
+++ Controller/perimeterController.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../Model/areaPerimeter.php';
+
+
+$base = $_POST['base'];
+$altura = $_POST['altura'];
+
+$resultPerimeter = new Perimeter($base,$altura);
+
+echo $resultPerimeter->getBase() ."<br>";
+
+echo $resultPerimeter->getAltura(). "<br>";
+
+
+echo "resultado de rectangulo <br>";
+
+echo "Area ".$resultPerimeter->areaRectangulo() ."<br>";
+
+echo "Perimetro". $resultPerimeter->CalculatePerimeterRectangulo();
+
+
---
+++ Model/areaPerimeter.php
@@ -0,0 +1,55 @@
+<?php
+
+Class Perimeter{
+    private $base;
+    private $altura;
+    public function __construct($base , $altura){
+        $this->base = $base;
+        $this->altura = $altura;
+    }
+
+    //getters
+
+    public function getBase()
+    {
+        return $this->base;
+    }
+       
+    public function getAltura()
+    {
+        return $this->altura;
+    }
+
+    //setters
+
+    public function setBase($base)
+    {
+        $this->base = $base;
+
+        return $this;
+    }
+ 
+    public function setAltura($altura)
+    {
+        $this->altura = $altura;
+
+        return $this;
+    }
+
+    // own function 
+    
+    //Rectangulo
+
+    public function areaRectangulo(){
+        $area= ($this->base * $this->altura);        
+        return $area;
+    }
+
+    public function CalculatePerimeterRectangulo(){
+        $b = $this->base;
+        $a =  $this->altura;
+        $resultR = 2 * ($b + $a);
+        return $resultR;
+    }
+
+}
